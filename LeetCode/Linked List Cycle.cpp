cd0f1012c2542b0bfc57ae453b7642178f954c68
Maxwell-L <974230257@qq.com>
2019-02-18 18:00:50 +0800

    Create Linked List Cycle.cpp

---
+++ LeetCode/Linked List Cycle.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode *slow = head;
+        ListNode *fast = head;
+        while(fast != NULL){
+            slow = slow -> next;
+            fast = fast -> next == NULL ? NULL : fast -> next -> next;
+            if(fast == NULL){
+                return false;
+            }
+            if(slow == fast){
+                return true;
+            }
+        }
+        return false;
+    }
+};
