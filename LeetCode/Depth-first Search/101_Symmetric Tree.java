0f2ee9bb20c722be5bed4877c1397206e9e9ad53
Maxwell-L <974230257@qq.com>
2019-05-22 17:00:40 +0800

    Update

--- LeetCode/Depth-first Search/101_Symmetric Tree.java
+++ LeetCode/Depth-first Search/101_Symmetric Tree.java
@@ -1,4 +1,23 @@
 /**
+ * 101.Symmetric Tree
+ *     Easy
+ * Description:
+ * Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+ * For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+ *         1
+ *        / \
+ *       2   2
+ *      / \ / \
+ *     3  4 4  3
+ * But the following [1,2,2,null,3,null,3] is not:
+ *   	   1
+ *        / \
+ *       2   2
+ *        \   \
+ *        3    3
+ */
+ 
+/**
  * Definition for a binary tree node.
  * public class TreeNode {
  *     int val;
