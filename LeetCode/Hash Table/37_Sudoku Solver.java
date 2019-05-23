2e20bd03fa0775361c1091515e5535e1c83df4e1
Maxwell-L <974230257@qq.com>
2019-05-23 17:38:51 +0800

    Update

--- LeetCode/Hash Table/37_Sudoku Solver.java
+++ LeetCode/Hash Table/37_Sudoku Solver.java
@@ -1,3 +1,15 @@
+/**
+ * 37.Sudoku Solver
+ *    Hard
+ * Description:
+ * Write a program to solve a Sudoku puzzle by filling the empty cells.
+ * A sudoku solution must satisfy all of the following rules:
+ * 	Each of the digits 1-9 must occur exactly once in each row.
+ * 	Each of the digits 1-9 must occur exactly once in each column.
+ * 	Each of the the digits 1-9 must occur exactly once in each of the 9 3x3 sub-boxes of the grid.
+ * Empty cells are indicated by the character '.'.
+ */
+
 class Solution {
     private void sudoku(char[][] board, boolean[][] row, boolean[][] col, int[] isFull, int nextRow, int nextCol){
         if(isFull[0] == 81){
