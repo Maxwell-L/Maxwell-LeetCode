7bdecb34142e97373b71a0b6145e43f3d1c25f98
Maxwell-L <974230257@qq.com>
2019-05-24 21:29:14 +0800

    Update 21_Merge Two Sorted Lists.java

--- LeetCode/Linked List/21_Merge Two Sorted Lists.java
+++ LeetCode/Linked List/21_Merge Two Sorted Lists.java
@@ -1,6 +1,6 @@
 /**
  * 21.Merge Two Sorted Lists
- * Easy
+ *    Easy
  * Description:
  * Merge two sorted linked lists and return it as a new list. 
  * The new list should be made by splicing together the nodes of the first two lists.
