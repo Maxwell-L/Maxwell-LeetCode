6cf857206f81c6e86d321fd5542d1bf318246431
Maxwell-L <974230257@qq.com>
2019-08-28 22:32:46 +0800

    Update 264_Ugly Number II.java

--- LeetCode/Dynamic Programming/264_Ugly Number II.java
+++ LeetCode/Dynamic Programming/264_Ugly Number II.java
@@ -1,6 +1,6 @@
 /**
  * 264.Ugly Number II
- * Medium
+ *     Medium
  * Description:
  * Write a program to find the n-th ugly number.
  * Ugly numbers are positive numbers whose prime factors only include 2, 3, 5. 
